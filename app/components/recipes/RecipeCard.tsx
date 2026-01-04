cec3042bfb65a71386f5d4a1d78a0135eb9cafdf
Artem Vyshnevskiy <thecrowpath@yandex.ru>
2026-01-04 23:06:20 +0300

    RecipeCard.tsx переведен на использование Flowbite Card

--- app/components/recipes/RecipeCard.tsx
+++ app/components/recipes/RecipeCard.tsx
@@ -6,6 +6,7 @@ import { HeartIcon as SolidHeartIcon } from "@heroicons/react/24/solid"
 import Link from "next/link"
 import { Ingredient } from "@/app/generated/prisma"
 import Rating from "./Rating"
+import { Card } from "flowbite-react"
 
 export default function RecipeCard({ id, title, description, imageUrl, rating, chef, tags }: RecipeWithRelations) {
   const [liked, setLiked] = useState(false)
@@ -69,99 +70,97 @@ export default function RecipeCard({ id, title, description, imageUrl, rating, c
   }
 
   return (
-    <li
-      className="rounded-lg overflow-hidden shadow flex flex-col dark:bg-gray-800"
-      key={id}
-    >
-      <div className="relative">
+    <Card
+      className="w-full overflow-hidden relative"
+      renderImage={() => (
         <Link href={`/recipes/${id}`}>
           <Image
+            width={640}
+            height={427}
             src={imageUrl}
             alt={title}
-            width="640"
-            height="427"
           />
         </Link>
-        <button
-          onClick={toggleLike}
-          className="absolute top-2 end-2 p-2 cursor-pointer"
-        >
-          {liked ? (
-            <SolidHeartIcon
-              width="24"
-              height="24"
-              className="text-red-500"
-            />
-          ) : (
-            <SolidHeartIcon
-              width="24"
-              height="24"
-              className="text-white"
-            />
-          )}
-        </button>
-      </div>
-      <div className="p-4 flex flex-col justify-between gap-4 grow">
-        <div>
-          <h2 className="text-xl font-semibold mb-2">
-            <Link href={`/recipes/${id}`}>{title}</Link>
-          </h2>
-          <div className="mb-2">
-            <Rating value={rating} />
-          </div>
-          <p className="text-sm text-gray-700 dark:text-gray-400 mb-2">{description}</p>
-          {!ingredientsShown ? (
-            <button
-              className="text-blue-500 hover:text-blue-400 text-sm cursor-pointer"
-              onClick={showIngredients}
-            >
-              Показать ингредиенты
-            </button>
-          ) : (
-            <>
-              <ul className="text-sm text-gray-500 flex flex-col gap-1">
-                {isLoadingIngredients
-                  ? Array(4)
-                      .fill(null)
-                      .map((item, i) => (
-                        <li key={i}>
-                          <span className="bg-gray-100 block w-full h-5 rounded-sm animate-pulse"></span>
-                        </li>
-                      ))
-                  : ingredients.map((ingredient) => (
-                      <li
-                        key={ingredient.id}
-                        className="flex justify-between"
-                      >
-                        <span>{ingredient.name}</span> <span>{ingredient.amount}</span>
+      )}
+    >
+      <button
+        onClick={toggleLike}
+        className="absolute top-2 end-2 p-2 cursor-pointer"
+      >
+        {liked ? (
+          <SolidHeartIcon
+            width="24"
+            height="24"
+            className="text-red-500"
+          />
+        ) : (
+          <SolidHeartIcon
+            width="24"
+            height="24"
+            className="text-white"
+          />
+        )}
+      </button>
+      <div className="flex h-full flex-col gap-4">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <Link href={`/recipes/${id}`}>{title}</Link>
+        </h2>
+
+        <Rating value={rating} />
+
+        <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+
+        {!ingredientsShown ? (
+          <button
+            className="text-blue-500 hover:text-blue-400 text-sm cursor-pointer text-start"
+            onClick={showIngredients}
+          >
+            Показать ингредиенты
+          </button>
+        ) : (
+          <>
+            <ul className="text-sm text-gray-500 flex flex-col gap-1">
+              {isLoadingIngredients
+                ? Array(4)
+                    .fill(null)
+                    .map((item, i) => (
+                      <li key={i}>
+                        <span className="bg-gray-100 dark:bg-gray-700 block w-full h-5 rounded-sm animate-pulse"></span>
                       </li>
-                    ))}
-              </ul>
-              <button
-                className="text-blue-500 hover:text-blue-400 text-sm cursor-pointer"
-                onClick={hideIngredients}
-              >
-                Скрыть ингредиенты
-              </button>
-            </>
-          )}
-        </div>
-        <div className="flex flex-col gap-2">
-          {chef && <div className="text-sm text-gray-600 dark:text-gray-500">{chef.name}</div>}
-          {tags && tags.length > 0 && (
-            <ul className="flex gap-2 flex-wrap">
-              {tags.map((tag) => (
-                <li
-                  key={tag.tagId}
-                  className="px-2 py-1 text-sm rounded bg-gray-100 dark:bg-gray-700"
-                >
-                  #{tag.tag.name}
-                </li>
-              ))}
+                    ))
+                : ingredients.map((ingredient) => (
+                    <li
+                      key={ingredient.id}
+                      className="flex justify-between"
+                    >
+                      <span>{ingredient.name}</span> <span>{ingredient.amount}</span>
+                    </li>
+                  ))}
             </ul>
-          )}
-        </div>
+            <button
+              className="text-blue-500 hover:text-blue-400 text-sm cursor-pointer text-start"
+              onClick={hideIngredients}
+            >
+              Скрыть ингредиенты
+            </button>
+          </>
+        )}
       </div>
-    </li>
+      <div className="flex flex-col gap-2">
+        {chef && <div className="text-sm text-gray-600 dark:text-gray-500">{chef.name}</div>}
+        {tags && tags.length > 0 && (
+          <ul className="flex gap-2 flex-wrap">
+            {tags.map((tag) => (
+              <li
+                key={tag.tagId}
+                className="px-2 py-1 text-sm rounded bg-gray-100 dark:bg-gray-700"
+              >
+                #{tag.tag.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </Card>
   )
 }
--- app/components/recipes/RecipeList.tsx
+++ app/components/recipes/RecipeList.tsx
@@ -6,13 +6,13 @@ interface RecipeListProps {
 
 export default function RecipeList({ recipes }: RecipeListProps) {
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {recipes.map((recipe: RecipeWithRelations) => (
         <RecipeCard
           key={recipe.id}
           {...recipe}
         />
       ))}
-    </ul>
+    </div>
   )
 }
