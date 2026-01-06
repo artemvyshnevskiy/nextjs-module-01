f57ca023fe43652f993355b2f4d53fc2bce17de6
Artem Vyshnevskiy <thecrowpath@yandex.ru>
2026-01-06 14:17:17 +0300

    Добавлена форма авторизации с использованием react-hook-form

---
+++ app/auth/login/_components/LoginForm2.tsx
@@ -0,0 +1,85 @@
+"use client"
+
+import Link from "next/link"
+import { TextInput, Label, Checkbox, Button, HelperText } from "flowbite-react"
+import { loginFormSchema, loginFormSchemaType } from "../_model/schema"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginFormSchemaType>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+
+  const onSubmit = (data: loginFormSchemaType) => {
+    console.log(data)
+  }
+
+  return (
+    <form
+      className="flex flex-col gap-4 md:gap-6"
+      action="#"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div>
+        <div className="mb-2">
+          <Label htmlFor="email">Email</Label>
+        </div>
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="name@company.com"
+          required
+          {...register("email")}
+          color={errors.email ? "failure" : undefined}
+        />
+        {errors.email && <HelperText color="failure">{errors.email.message}</HelperText>}
+      </div>
+
+      <div>
+        <div className="mb-2">
+          <Label htmlFor="password">Пароль</Label>
+        </div>
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="••••••••"
+          required
+          {...register("password")}
+          color={errors.password ? "failure" : undefined}
+        />
+        {errors.password && <HelperText color="failure">{errors.password.message}</HelperText>}
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Checkbox id="remember" />
+        <Label htmlFor="remember">Запомнить меня</Label>
+      </div>
+
+      <Button
+        type="submit"
+        className="cursor-pointer"
+      >
+        Войти
+      </Button>
+
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        Ещё нет аккаунта?{" "}
+        <Link
+          href="/auth/registration"
+          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >
+          Зарегистрироваться
+        </Link>
+      </p>
+    </form>
+  )
+}
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,4 @@
-import LoginForm from "./_components/LoginForm"
+import LoginForm2 from "./_components/LoginForm"
 
 export default function LoginPage() {
   return (
@@ -6,7 +6,7 @@ export default function LoginPage() {
       <div className="mx-auto w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Войти в аккаунт</h1>
-          <LoginForm />
+          <LoginForm2 />
         </div>
       </div>
     </section>
